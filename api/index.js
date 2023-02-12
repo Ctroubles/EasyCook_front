4af8b041a6c652e073c1c169b9a1ad58f9ba148b
troubles <cesar.troubles@gmail.com>
2023-02-11 20:58:00 -0500

    apregando lo que le faltaba al .env

--- api/index.js
+++ api/index.js
@@ -19,6 +19,8 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config();
+
 
 const {port} = process.env;
 
