03f070a7c264306e190f97156c1cac97dc86544c
troubles <cesar.troubles@gmail.com>
2023-02-20 10:58:27 -0500

    remove {force:true}

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ require('dotenv').config();
 const {PORT} = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync().then(() => {
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducer/reducer';
 import thunkMiddlware from "redux-thunk"
 
-const composeEnhancer= window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancer = composeEnhancer(applyMiddleware(thunkMiddlware));
 
