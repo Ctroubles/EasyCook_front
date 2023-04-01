313e1921b1a72bb364185cc8a0036b10f9a1ce0d
Ctroubles <95973230+Ctroubles@users.noreply.github.com>
2023-03-31 20:47:11 -0500

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -15,8 +15,9 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://easy-cook-ctroubles.vercel.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "*",
+  "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
